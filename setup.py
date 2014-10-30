fb4ea427405ee88e2d652e364ef7d36ab0739148
Vladimir Vitvitskiy <volodymyr.vitvitskiy@fun>
2014-10-30 17:38:25 +0200

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_content_of(path):
 if __name__ == '__main__':
     setuptools.setup(
         name='rest-gae',
-        version="0.1",
+        version="1.2",
         description='RiverMeadow Messagebus',
         long_description=get_content_of('README.md'),
         packages=setuptools.find_packages(exclude=['tests']),
