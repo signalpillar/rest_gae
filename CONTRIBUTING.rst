a0bdff02fda419b582ce5350b386762dd9c46ff1
Vladimir Vitvitskiy <volodymyr.vitvitskiy@fun>
2014-10-30 17:24:00 +0200

    add driver file for the destribution purposes

---
+++ CONTRIBUTING.rst
@@ -0,0 +1,4 @@
+Style Guide
+~~~~~~~~~~~
+
+``rest-gae`` aims to follow PEP8 including 4 space indents and 100 character line limits.
---
+++ setup.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+import setuptools
+
+
+def get_content_of(path):
+    """Read contents of file
+
+    :path: file path
+    :return: file contents
+    :rtype: str
+    """
+    with open(path, 'r') as f:
+        return f.read()
+
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='rest-gae',
+        version="0.1",
+        description='RiverMeadow Messagebus',
+        long_description=get_content_of('README.md'),
+        packages=setuptools.find_packages(exclude=['tests']),
+    )
